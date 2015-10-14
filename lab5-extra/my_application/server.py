012a6eb520aeaf3d57b89886336afa6990e2f40b
André Barata <andre.quina.barata@gmail.com>
2015-10-14 18:32:15 +0300

    Finishing extra

--- lab5-extra/my_application/server.py
+++ lab5-extra/my_application/server.py
@@ -3,26 +3,42 @@ app = Flask(__name__)
 
 @app.route("/paledrome/<word>")
 def paledrome(word):
-    return (str(word[::-1] == word))
+  return (str(word[::-1] == word))
 
 @app.route("/fibonacci")
 def fibonacci():
-    total = 0
+  total = 0
 
-    old = 1
-    new = 1
+  old = 1
+  new = 1
 
-    while (old < 4000000 ):
-        next = old + new
+  while (old < 4000000 ):
+      next = old + new
 
-        if (next % 2 == 0):
-            total += next
+      if (next % 2 == 0):
+          total += next
 
-        old = new
-        new = next
+      old = new
+      new = next
 
-    return (str(total))
+  return (str(total))
 
+@app.route("/multiples/<int:end>")
+def multiples(end):
+  total = 0
+  number = 0
+
+  #Count to 1000
+  while (number < end):
+    #Increment if it is a multiple of 5 or 3
+    if (number % 5 == 0 or number % 3 == 0):
+      #Add to total
+      total += number
+
+    #Increment number
+    number+=1
+
+  return(str(total))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
